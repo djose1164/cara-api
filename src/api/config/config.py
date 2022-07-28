bc5484e7dfb6ae99696c5aaf1f5c3448cde531aa
Daniel Victoriano <djose1164@gmail.com>
2022-07-27 23:44:49 -0400

    Fix security leaks on config

--- src/api/config/config.py
+++ src/api/config/config.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 class Config:
@@ -7,12 +11,10 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     JWT_SECRET_KEY = os.environ.get("MY_FLASK_SECRET")
-    SECRET_KEY = "do_not_ask"
-    SECURITY_PASSWORD_SALT = "do_not_ask_me"
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECURITY_PASSWORD_SALT = os.environ.get("SECURITY_PASSWORD_SALT")
 
-    SQLALCHEMY_DATABASE_URI = (
-        "mysql+pymysql://djose1164:Kirito08.@db4free.net/test_cara"
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
 
 
 class ProductionConfig(Config):
