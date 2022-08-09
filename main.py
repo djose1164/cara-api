6100751a29dedbca75696a005b5ad1d2181afcab
Daniel Victoriano <djose1164@gmail.com>
2022-08-09 18:26:18 -0400

    Add order route to app.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from flask_jwt_extended import JWTManager
 
 from api.routes.users import user_routes
 from api.routes.products import product_routes
+from api.routes.orders import order_routes
 from api.config.config import ProductionConfig, TestingConfig, DevelopmentConfig
 import api.utils.responses as resp
 from api.utils.responses import response_with
@@ -26,6 +27,7 @@ app.config.from_object(app_config)
 
 app.register_blueprint(user_routes, url_prefix="/api/users")
 app.register_blueprint(product_routes, url_prefix="/api/products")
+app.register_blueprint(order_routes, url_prefix="/api/orders")
 
 
 @app.route("/")
@@ -65,6 +67,7 @@ logging.basicConfig(
     stream=sys.stdout,
     format="%(asctime)s|%(levelname)s|%(filename)s:%(lineno)s|%(message)s",
     level=logging.DEBUG,
+    filemode="cara-api.log",
 )
 
 if __name__ == "__main__":
