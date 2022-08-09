5afb51809e26a6b63bf7c982c43e8b746b1990f4
Daniel Victoriano <djose1164@gmail.com>
2022-08-09 19:53:26 -0400

    Add more gets.

--- api/routes/orders.py
+++ api/routes/orders.py
@@ -1,15 +1,31 @@
 from flask import Blueprint, request
 
 from api.models.orders import Order, OrderSchema
+from api.models.clients import Client
 from api.utils.responses import response_with
 import api.utils.responses as resp
 
 order_routes = Blueprint("order_routes", __name__)
 
+
 @order_routes.route("/")
 def order_index():
     fetched = Order.query.all()
-    print(f"fetched: {fetched}")
     fetched = OrderSchema().dump(fetched, many=True)
-    print(f"fetched: {fetched}")    
     return response_with(resp.SUCCESS_200, value={"orders": fetched})
+
+
+@order_routes.route("/<id>")
+def get_order_by_id(id):
+    if not id.isdecimal():
+        return get_order_by_client(id)
+    fetched = Order.query.filter_by(id=id).first_or_404()
+    fetched = OrderSchema().dump(fetched)
+    return response_with(resp.SUCCESS_200, value={"order": fetched})
+
+
+def get_order_by_client(name):
+    client = Client.query.filter_by(name=name).first_or_404()
+    fetched = Order.query.filter_by(client_id=client.id).first_or_404()
+    fetched = OrderSchema().dump(fetched)
+    return response_with(resp.SUCCESS_200, value={"order": fetched})
