35e6d55a97169f5a804bb7a553637fee260a0ad9
Daniel Victoriano <djose1164@gmail.com>
2022-08-13 16:33:18 -0400

    Update

--- api/models/orders.py
+++ api/models/orders.py
@@ -2,31 +2,46 @@ from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from marshmallow import fields
 
 from api.utils.database import db
+from api.models.payments import PaymentSchema
+from api.models.order_details import OrderDetailSchema
+
 
 class Order(db.Model):
     __tablename__ = "orders"
     id = db.Column(db.Integer, nullable=False, primary_key=True)
-    payment_status = db.Column(db.Integer)
     date = db.Column(db.Date, server_default=db.func.now())
     client_id = db.Column(db.Integer, db.ForeignKey("clients.id"), nullable=False)
-    
-    def __init__(self, payment_status, client_id):
-        self.payment_status = payment_status
+    payments = db.relationship("Payment", backref="order")
+    order_details = db.relationship("OrderDetail", backref="order")
+
+    def __init__(self, client_id, date = None, payments=None, order_details=None):
+        if order_details is None:
+            order_details = list()
+        if payments is None:
+            payments = list()
         self.client_id = client_id
-        
+        self.date = date
+        self.payments = payments
+        self.order_details = order_details
+
     def create(self):
         db.session.add(self)
         db.session.commit()
         return self
-    
+
+
 class OrderSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Order
         load_instance = True
         sqla_session = db.session
-    
+
     id = fields.Integer(dump_only=True)
-    payment_status = fields.Boolean(required=True)
     date = fields.Date()
     client_id = fields.Integer(required=True)
-        
+    payments = fields.Nested(
+        PaymentSchema, many=True, only=["status", "paid_amount", "amount_to_pay"]
+    )
+    order_details = fields.Nested(
+        OrderDetailSchema, many=True, only=["quantity", "unit_price"]
+    )
