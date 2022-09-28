ee122a20786b4ea27fd26f5cb33c5456eed63c27
Daniel Victoriano <djose1164@gmail.com>
2022-09-28 18:28:25 -0400

    Can add new product to catalog.

--- api/models/products.py
+++ api/models/products.py
@@ -21,11 +21,13 @@ class Product(db.Model):
     __tablename__ = "products"
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(64), nullable=False)
+    description = db.Column(db.String(64))
     buy_price = db.Column(db.Integer, nullable=False)
     sell_price = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, name, buy_price, sell_price):
+    def __init__(self, name, description, buy_price, sell_price):
         self.name = name
+        self.description = description
         self.buy_price = buy_price
         self.sell_price = sell_price
 
@@ -51,5 +53,6 @@ class ProductSchema(SQLAlchemyAutoSchema):
 
     id = fields.Integer(dump_only=True)
     name = fields.String(required=True)
+    description = fields.String(required=True)
     buy_price = fields.Integer(required=True)
     sell_price = fields.Integer(required=True)
