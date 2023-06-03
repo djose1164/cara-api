c85320f93b9ebc7a25dda22de8bdac034094e826
Daniel Victoriano <djose1164@gmail.com>
2023-06-03 11:43:47 -0400

    Add method to find status by id

--- api/models/order_status.py
+++ api/models/order_status.py
@@ -7,14 +7,17 @@ class OrderStatus(db.Model):
     __tablename__ = "order_status"
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     status = db.Column(db.String(32), nullable="False", unique=True)
-    
-    
+
+    @classmethod
+    def find_status_by_id(cls, status_id: int):
+        return cls.query.filter_by(id=status_id).first_or_404()
+
+
 class OrderStatusSchema(SQLAlchemyAutoSchema):
     class Meta:
         sqla_session = db.session
         load_instance = True
         model = OrderStatus
-        
+
     id = auto_field(dump_only=True)
     status = auto_field()
-        
