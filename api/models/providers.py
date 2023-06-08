db4547c0f43a78c6ee647760fad8b1a631da0dc7
Daniel Victoriano <djose1164@gmail.com>
2023-06-08 16:23:01 -0400

    Add providers model

---
+++ api/models/providers.py
@@ -0,0 +1,29 @@
+from marshmallow_sqlalchemy import SQLAlchemySchema, auto_field, fields
+from api.models.contact import ContactSchema
+from api.utils.database import db
+
+
+class Provider(db.Model):
+    __tablename__ = "providers"
+
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    contact_id = db.Column(db.Integer, db.ForeignKey("contacts.id"))
+    contact = db.relationship("Contact", backref="contact", uselist=False)
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+
+class ProviderSchema(SQLAlchemySchema):
+    class Meta:
+        model = Provider
+        sqla_session = db.session
+        load_instance = True
+
+    id = auto_field(dump_only=True)
+    name = auto_field(required=True)
+    contact = fields.Nested(ContactSchema)
+    
