28e1c894ebc7327c141294355fc5141a689f3ebb
Daniel Victoriano <djose1164@gmail.com>
2023-06-08 16:21:37 -0400

    Add providers route

---
+++ api/routes/providers.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, request
+from marshmallow import EXCLUDE
+from api.models.contact import ContactSchema
+
+from api.models.providers import Provider, ProviderSchema
+from api.utils.responses import response_with
+import api.utils.responses as resp
+from api.utils.database import db
+
+
+provider_routes = Blueprint("provider_routes", __name__)
+
+
+@provider_routes.route("/")
+def provider_index():
+    fetched = Provider.query.all()
+    providers = ProviderSchema().dump(fetched, many=True)
+    return response_with(resp.SUCCESS_200, value={"providers": providers})
+
+
+@provider_routes.route("/", methods=["POST"])
+def create_provider():
+    try:
+        data = request.get_json()
+
+        provider = ProviderSchema(unknown=EXCLUDE).load(data, partial=True)
+        provider.create()
+        
+        return response_with(resp.SUCCESS_200)        
+    except Exception as e:
+        print("While creating provider: ", e)
+        return response_with(resp.BAD_REQUEST_400)
+
