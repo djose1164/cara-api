8d0ec444db22fd902e1e6d3881dad7ef1d0a83cc
Daniel Victoriano <djose1164@gmail.com>
2023-06-10 16:26:14 -0400

    Apply changes to be loadable

--- api/models/orders.py
+++ api/models/orders.py
@@ -1,5 +1,5 @@
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-from marshmallow import fields
+from marshmallow import EXCLUDE, fields
 
 from api.utils.database import db
 from api.models.payments import PaymentSchema
@@ -35,14 +35,13 @@ class OrderSchema(SQLAlchemyAutoSchema):
         model = Order
         load_instance = True
         sqla_session = db.session
+        include_fk = True
 
     id = fields.Integer(dump_only=True)
     date = fields.Date(format="%d/%m/%Y", load_default=db.func.now())
     customer_id = fields.Integer(required=True)
-    payment = fields.Nested(
-        PaymentSchema, many=False, only=("paid_amount", "amount_to_pay", "status", "id")
-    )
+    payment = fields.Nested(PaymentSchema)
     order_details = fields.Nested(
-        OrderDetailSchema, many=True, only=("product", "quantity")
+        OrderDetailSchema, many=True, exclude=("order_id",), unknown=EXCLUDE
     )
-    order_status = fields.Nested(OrderStatusSchema)
+    order_status = fields.Nested(OrderStatusSchema, dump_only=True)
