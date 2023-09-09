f4621364634dbc830f559da0b30d87e6cfe658bc
Daniel Victoriano <djose1164@gmail.com>
2023-09-09 17:40:25 -0400

    Accept product_id to see available stocks

--- api/routes/inventory.py
+++ api/routes/inventory.py
@@ -11,19 +11,22 @@ inventory_routes = Blueprint("inventory_routes", __name__)
 
 
 @inventory_routes.route("/")
-#@jwt_required()
+@jwt_required()
 def index():
     admin_id = request.args.get("admin_id")
+    product_id = request.args.get("product_id")
     if admin_id:
         fetched = Inventory.find_inventory_by_admin_id(admin_id)
         fetched = InventorySchema(many=True).dump(fetched)
         return response_with(resp.SUCCESS_200, value={"inventory": fetched})
+    elif product_id is not None:
+        return avaivable_stocks_of(product_id)
         
     return resp.response_with(resp.BAD_REQUEST_400)
 
 
 @inventory_routes.route("/<int:inventory_id>", methods=["PUT"])
-#@jwt_required()
+@jwt_required()
 def put_inventory(inventory_id: int):
     try:
         data = request.get_json()
@@ -40,3 +43,9 @@ def put_inventory(inventory_id: int):
     except Exception as e:
         print(e)
         return response_with(resp.INVALID_INPUT_422)
+    
+
+def avaivable_stocks_of(product_id: int):
+    inventories = Inventory.query.filter_by(product_id=product_id).all()
+    inventories = InventorySchema(exclude=("product",), many=True).dump(inventories)
+    return response_with(resp.SUCCESS_200, value={"inventories": inventories})
