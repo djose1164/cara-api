6e79e62dcba95643301a69c45343aff56ca029dd
Daniel Victoriano <djose1164@gmail.com>
2023-09-09 19:35:55 -0400

    Update inventory model

--- api/models/inventory.py
+++ api/models/inventory.py
@@ -2,17 +2,21 @@ from marshmallow import fields
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from api.models.products import ProductSchema
 
-from api.models.stocks import StocksSchema
+from api.models.warehouse import WarehouseSchema
 from api.utils.database import db
 
 
 class Inventory(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=True)
+    quantity_available = db.Column(db.Integer, default=0, nullable=False)
+    minimun_stock_level = db.Column(db.Integer, default=5, nullable=False)
+    maximun_stock_level = db.Column(db.Integer, default=30, nullable=False)
+    reorder_point = db.Column(db.Integer, default=18, nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
-    stock_id = db.Column(db.Integer, db.ForeignKey("stocks.id"), unique=True)
+    warehouse_id = db.Column(db.Integer, db.ForeignKey("warehouse.id"), unique=True)
     admin_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     product = db.relationship("Product", backref="inventory_product")
-    stocks = db.relationship("Stocks", backref="inventory_stocks")
+    warehouse = db.relationship("Warehouse", backref="inventory_warehouse")
 
     @classmethod
     def find_inventory_by_admin_id(cls, admin_id: int):
@@ -29,6 +33,9 @@ class Inventory(db.Model):
     @classmethod
     def find_inventory_by_id(cls, inventory_id: int):
         return cls.query.filter_by(id=inventory_id).one()
+    
+    def enough_stocks_for(self, quantity: int) -> bool:
+        return self.quantity_available >= quantity
 
 
 class InventorySchema(SQLAlchemyAutoSchema):
@@ -38,4 +45,4 @@ class InventorySchema(SQLAlchemyAutoSchema):
         sqla_session = db.session
 
     product = fields.Nested(ProductSchema)
-    stocks = fields.Nested(StocksSchema)
+    warehouse = fields.Nested(WarehouseSchema)
