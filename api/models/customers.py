46a41d9cc65d90d0c112fb9dded1cf2049c7be5e
Daniel Victoriano <djose1164@gmail.com>
2023-09-09 19:19:28 -0400

    Remove things releated to order

--- api/models/customers.py
+++ api/models/customers.py
@@ -3,9 +3,7 @@ Copyright Cara Daniel Victoriano 2022
 """
 from marshmallow import fields
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
-from sqlalchemy.orm import contains_eager
 
-from api.models.orders import OrderSchema, Order
 from api.models.person_info import PersonInfoSchema
 from api.utils.database import db
 
@@ -14,9 +12,7 @@ class Customer(db.Model):
     __tablename__ = "customers"
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     admin_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    orders = db.relationship(
-        "Order", backref="customer", order_by="Order.date, Order.id"
-    )
+
     person_info = db.relationship("PersonInfo", backref="customer", uselist=False)
     
 
@@ -32,17 +28,6 @@ class Customer(db.Model):
     @classmethod
     def find_by_name(cls, name):
         return cls.query.filter(cls.name.like(f"%{name}%")).all()
-
-    @classmethod
-    def filter_by_payment_status(cls, status):
-        return (
-            cls.query.join(cls.orders)
-            .join(Order.payment)
-            .options(contains_eager(cls.orders))
-            .filter_by(status=status)
-            .populate_existing()
-            .all()
-        )
         
     @classmethod
     def customers_by_admin_id(cls, admin_id: int):
@@ -63,6 +48,7 @@ class CustomerSchema(SQLAlchemyAutoSchema):
         sqla_session = db.session
 
     id = auto_field(dump_only=True)
-    orders = fields.Nested(OrderSchema, many=True, exclude=("customer_id",))
     person_info = fields.Nested(PersonInfoSchema)
     admin_id = auto_field()
+    customer_id = fields.Function(lambda obj: obj.id)
+    name = fields.Function(lambda obj: obj.person_info.forename + " " + obj.person_info.surname)
