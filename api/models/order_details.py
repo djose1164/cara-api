8ea17fd25c18e87885c1bb5610cec7206e9db2d5
Daniel Victoriano <djose1164@gmail.com>
2023-09-25 16:57:53 -0400

    Improve validation

--- api/models/order_details.py
+++ api/models/order_details.py
@@ -1,6 +1,7 @@
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from marshmallow import fields
 from api.models.inventory import Inventory
+from api.models.products import Product
 
 from api.utils.database import db
 from api.utils.exceptions import StocksException
@@ -19,44 +20,47 @@ class OrderDetail(db.Model):
         db.session.add(self)
         db.session.commit()
         return self
-
+    
     @staticmethod
-    def validate_product_stocks(details: dict, admin_id: int=None):
+    def validate_admin_order(admin_id: int, details: dict):
         for detail in details:
             product_id: int = detail["product_id"]
-            if admin_id is not None:
-                inventory: Inventory = Inventory.find_inventory(
-                    admin_id, product_id
-                )
+            inventory: Inventory = Inventory.find_inventory(admin_id, product_id)
+            quantity: int = detail["quantity"]
+
+            if not inventory.enough_stocks_for(quantity):
+                raise StocksException(inventory.product.name)
             else:
-                inventories: list[Inventory] = Inventory.find_inventories_with_stocks_for(product_id)
-            
-            missing_quantity = quantity = detail["quantity"]
+                inventory.quantity_available -= quantity
 
-            for inventory in inventories:
-                stock_available = inventory.quantity_available
-                if stock_available == 0:
-                    continue
+        db.session.add(inventory)
+        db.session.flush()
 
-                if stock_available == missing_quantity:
-                    inventory.quantity_available = 0
-                    break
-                elif missing_quantity > stock_available:
-                    missing_quantity -= stock_available
-                    inventory.quantity_available = 0
-                else:
-                    inventory.quantity_available -= missing_quantity
-                    missing_quantity = 0
-                    break
-            else:
-                product_name = inventory.product.name
-                raise StocksException(product_name)
+    @staticmethod
+    def validate_customer_order(details: dict):
+        for detail in details:
+            product_id: int = detail["product_id"]
+            quantity: int = detail["quantity"]
+            OrderDetail.validate_across_inventories(product_id, quantity, detail)
+                        
+        
+    @staticmethod
+    def validate_across_inventories(product_id: int, required_quantity: int, detail: dict):
+        inventories: list[Inventory] = Inventory.find_inventories_with_stocks_for(product_id)
+        product_name = Product.find_product_by_id(product_id).name
+        for inventory in inventories:
+            stock_available = inventory.quantity_available
+            if stock_available == 0:
+                continue
 
-            detail["warehouse_id"] = inventory.warehouse_id
-                
-            db.session.add(inventory)
-            db.session.flush()
-
+            if stock_available >= required_quantity:
+                inventory.quantity_available -= required_quantity
+                detail["warehouse_id"] = inventory.warehouse_id
+                db.session.add(inventory)
+                db.session.flush()
+                break
+        else:
+            raise StocksException(product_name)
 
 class OrderDetailSchema(SQLAlchemyAutoSchema):
     class Meta:
