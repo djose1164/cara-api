38f7752171036c8c7b5967619d64e0998479ad8a
Daniel Victoriano <djose1164@gmail.com>
2023-10-11 10:55:52 -0400

    Add models for subdivisions

--- api/models/address.py
+++ api/models/address.py
@@ -1,16 +1,77 @@
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
 from marshmallow import fields
 
 from api.utils.database import db
 
+
+class Country(db.Model):
+    __tablename__ = "country"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(40), nullable=False)
+
+
+class CountrySchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Country
+        load_instance = True
+        sqla_session = db.session
+
+
+class Province(db.Model):
+    __tablename__ = "province"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(40), nullable=False)
+    country_id = db.Column(db.Integer, db.ForeignKey("country.id"))
+
+
+class ProvinceSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Province
+        load_instance = True
+        sqla_session = db.session
+
+
+class Municipality(db.Model):
+    __tablename__ = "municipality"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(40), nullable=False)
+    province_id = db.Column(db.Integer, db.ForeignKey("province.id"))
+
+
+class MunicipalitySchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Municipality
+        load_instance = True
+        sqla_session = db.session
+
+
+class Sector(db.Model):
+    __tablename__ = "sector"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(40), nullable=False)
+    municipality_id = db.Column(db.Integer, db.ForeignKey("municipality.id"))
+
+
+class SectorSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Sector
+        load_instance = True
+        sqla_session = db.session
+
+
 class Address(db.Model):
     __tablename__ = "address"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    country = db.Column(db.String(64))
-    province = db.Column(db.String(64))
-    municipality = db.Column(db.String(64))
+    country_id = db.Column(db.Integer, db.ForeignKey("country.id"))
+    province_id = db.Column(db.Integer, db.ForeignKey("province.id"))
+    municipality_id = db.Column(db.Integer, db.ForeignKey("municipality.id"))
+    sector_id = db.Column(db.Integer, db.ForeignKey("sector.id"))
     street = db.Column(db.String(64))
     house_number = db.Column(db.Integer)
+    country = db.relationship("Country", backref="country")
+    province = db.relationship("Province", backref="province")
+    municipality = db.relationship("Municipality", backref="municipality")
+    sector = db.relationship("Sector", backref="sector")
 
 
 class AddressSchema(SQLAlchemyAutoSchema):
@@ -18,10 +79,8 @@ class AddressSchema(SQLAlchemyAutoSchema):
         model = Address
         load_instance = True
         sqla_session = db.session
-        
-    country = fields.String()
-    province = fields.String()
-    municipality = fields.String()
-    street = fields.String()
-    house_number = fields.Integer()
-    
+
+    country_id = auto_field(required=True)
+    province_id = auto_field(required=True)
+    municipality_id = auto_field(required=True)
+    sector_id = auto_field(required=True)
