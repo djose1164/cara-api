3b82337d965c9845b028609bc23fbb7c2bd15954
Daniel Victoriano <djose1164@gmail.com>
2023-10-14 21:25:09 -0400

    Remove definer

--- scripts/statistics.sql
+++ scripts/statistics.sql
@@ -9,9 +9,9 @@ ORDER BY 1 DESC;
 create or replace VIEW bill_statistics 
 AS 
 select concat(`pi2`.`forename`,' ',`pi2`.`surname`) AS `name`,`pi2`.`customer_id` AS `customer_id`,count(`ps`.`status`) AS `bill_quantity`,`ps`.`status` AS `status`,sum(`p`.`paid_amount`) AS `paid_amount`,sum(`p`.`amount_to_pay`) AS `amount_to_pay` 
-from (((`cara_testing`.`person_info` `pi2` 
-join `cara_testing`.`orders` `o` on(`o`.`customer_id` = `pi2`.`customer_id`)) 
-join `cara_testing`.`payments` `p` on(`p`.`id` = `o`.`payment_id`))
-join `cara_testing`.`payment_status` `ps` on(`ps`.`id` = `p`.`payment_status_id`)) 
+from (((`person_info` `pi2` 
+join `orders` `o` on(`o`.`customer_id` = `pi2`.`customer_id`)) 
+join `payments` `p` on(`p`.`id` = `o`.`payment_id`))
+join `payment_status` `ps` on(`ps`.`id` = `p`.`payment_status_id`)) 
 group by `ps`.`status`,`pi2`.`customer_id` 
 order by `ps`.`status`,count(`ps`.`status`) desc,concat(`pi2`.`forename`,' ',`pi2`.`surname`);
