e90e49a1816ff1927c86bc2a270be9a4eca56071
Daniel Victoriano <djose1164@gmail.com>
2023-10-15 16:10:02 -0400

    Add InventoryNotFundException

--- api/utils/exceptions.py
+++ api/utils/exceptions.py
@@ -4,3 +4,11 @@ class StocksException(Exception):
             f"No hay suficiente cantidad disponible de {product_name} en inventario."
         )
         super().__init__(self.message)
+
+
+class InventoryNotFoundException(Exception):
+    def __init__(self, product_id, admin_id) -> None:
+        self.message = f"No existe ningún inventario para el productoID {product_id} bajo el adminID {admin_id}"
+        self.product_id = product_id
+        self.admin_id = admin_id
+        super().__init__(self.message)
