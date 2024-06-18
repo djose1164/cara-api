b055b77514dc254147105641e84f4933efa7c642
Daniel Victoriano <djose1164@gmail.com>
2024-06-18 10:04:44 -0400

    Rename table & move some columns to person

---
+++ api/models/person.py
@@ -0,0 +1,33 @@
+from api.models.address import AddressSchema
+from api.models.contact import ContactSchema
+from api.utils.database import db
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow import fields
+
+
+class Person(db.Model):
+    __tablename__ = "person"
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    forename = db.Column(db.String(32), nullable=False)
+    surname = db.Column(db.String(32))
+    birthday = db.Column(db.Date)
+    contact_id = db.Column(db.Integer, db.ForeignKey("contact.id"))
+    address_id = db.Column(db.Integer, db.ForeignKey("address.id"))
+    address = db.relationship("Address", backref="contact_address", uselist=False)
+    contact = db.relationship("Contact", backref="person", uselist=False)
+
+    @staticmethod
+    def find_person_by_id(person_id):
+        return db.get_or_404(Person, person_id)
+
+
+
+class PersonSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Person
+        sqla_session = db.session
+        load_instance = True
+
+    contact = fields.Nested(ContactSchema)
+    birthday = fields.DateTime()
+    address = fields.Nested(AddressSchema)
