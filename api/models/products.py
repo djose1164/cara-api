029783f8bb695907336f1cd2d3953fea43069f71
Daniel Victoriano <djose1164@gmail.com>
2024-10-13 18:05:13 -0400

    Improve use of PriceHistory

--- api/models/products.py
+++ api/models/products.py
@@ -7,7 +7,8 @@ from api.models.product_category import ProductCategorySchema
 from api.utils.database import db
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from marshmallow import fields
-from api.models.price_history import PriceHistory, PriceTypeEnum
+from api.models.price_history import PriceHistory, PriceHistorySchema, PriceTypeEnum
+from api.utils.exceptions import ResourceAlreadyExists
 
 
 class ProductImage(db.Model):
@@ -45,33 +46,50 @@ class Product(db.Model):
     )
     category = db.relationship("ProductCategory", backref="product_category")
     images = db.relationship("ProductImage", backref="product")
-    price_history = db.relationship(PriceHistory)
+    price_history = db.relationship(
+        PriceHistory,
+        primaryjoin="and_(PriceHistory.product_id==Product.id, PriceHistory.price_type=='sell')",
+    )
 
     def create(self):
         db.session.add(self)
         db.session.commit()
         return self
 
-    def invalidate_current_price(self):
-        for ph in self.price_history:
-            if ph.thru_date is None:
-                ph.invalidate_current_price()
-                break
+    def invalidate_current_price(self, price_type: PriceTypeEnum):
+        ph: PriceHistory = PriceHistory.get_latest_by_product_id(self.id, price_type)
+        ph.thru_date = datetime.now(timezone.utc)
+
+        #self.create()
 
     def set_current_price(self, new_price: PriceHistory):
-        self.invalidate_current_price()
+        if new_price.price == PriceHistory.get_latest_price(
+            self.id, new_price.price_type
+        ):
+            raise ResourceAlreadyExists("Ya existe una entrada con el mismo precio.")
+
+        if len(self.buy_price_history.all()) > 0:
+            self.invalidate_current_price(new_price.price_type)
+        self.create()
         self.price_history.append(new_price)
         self.create()
+
+    def get_current_supplier(self):
+        return PriceHistory.get_latest_by_product_id(self.id, PriceTypeEnum.BUY)
 
     @property
     def sell_price(self):
         return PriceHistory.get_latest_price(self.id, PriceTypeEnum.SELL)
-    
+
     @property
     def price(self):
         for ph in self.price_history:
             if ph.thru_date is None:
                 return ph.price
+
+    @property
+    def buy_price_history(self):
+        return PriceHistory.get_history_by_product_id(self.id, PriceTypeEnum.BUY)
 
     @classmethod
     def find_product_by_id(cls, id_) -> "Product":
@@ -98,9 +116,17 @@ class ProductSchema(SQLAlchemyAutoSchema):
     category_name = fields.Function(lambda obj: obj.category.name, dump_only=True)
     images = fields.List(fields.Nested("ProductImageSchema"))
     price_history = fields.List(fields.Nested("PriceHistorySchema"))
+    supplier_catalog = fields.Method("get_current_supplier", dump_only=True)
+    buy_price_history = fields.Method("get_buy_price_history", dump_only=True)
 
     def get_current_price(self, obj: Product):
         return obj.sell_price
-    
+
     def get_price(self, obj: Product):
         return obj.price
+
+    def get_current_supplier(self, obj: Product):
+        return PriceHistorySchema().dump(obj.get_current_supplier())
+
+    def get_buy_price_history(self, obj: Product):
+        return PriceHistorySchema(many=True).dump(obj.buy_price_history)
