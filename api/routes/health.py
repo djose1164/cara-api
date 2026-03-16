5bc3d4be7996ee86893eb64478e592639578c35a
Daniel Victoriano <djose1164@gmail.com>
2026-03-16 00:32:53 -0400

    update health.py

--- api/routes/health.py
+++ api/routes/health.py
@@ -13,11 +13,14 @@ health_routes = Blueprint("health_routes", __name__)
 @health_routes.route("/<version_code>")
 def status(version_code: str):
     maintenance_on = os.environ.get("MAINTENANCE_MODE")
+    maintenance_img_url = os.environ.get("MAINTENANCE_IMAGE_URL")
+    maintenance_msg = os.environ.get("MAINTENANCE_MESSAGE")
     lowest_supported_version = os.environ.get("CLIENT_LOWEST_SUPPORTED_VERSION")
+    
     if maintenance_on == "True":
         return response_with(
             resp.SERVER_ERROR_503,
-            message='<img src="https://cdn.dribbble.com/users/1801739/screenshots/15012625/media/e64203da9fdc4df221bfc8d79ab6ef0f.png?resize=360x0"><p>Estamos trabajando...</p>',
+            message=f'<img src="{maintenance_img_url}" height="200" width="200"><p>{maintenance_msg}</p>',
         )
     if Version(version_code) >= Version(lowest_supported_version):
         return response_with(resp.SUCCESS_200, value={"status": "OK"})
